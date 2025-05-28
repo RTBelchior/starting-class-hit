c57edb668e066c1d0bb9339d952718da92d2a663
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-05-28 12:18:38 +0100

    fim da aula 02 e inicio da aula 03

--- starting/README.md
+++ starting/README.md
@@ -14,3 +14,8 @@ component.css
 dentro da tag style={...}
 no arquivo index adicionamos as fontes do projeto
 feita a configuração conf. do css globsl no srquivo app.component.css
+obs: Inicio do conhecimento sobre API de rotas
+1º No arquivo app.routes.ts é p local da mossa configurações globais
+2º Falaremos sobre routlink="...." que fica dentro do componente header.html
+temos de importar seu "Module"
+Estudar JS Objetos e JS Arrays
--- starting/src/app/components/body/main/main.component.ts
+++ starting/src/app/components/body/main/main.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+    casa = {porta: 'porta azul', quantPorta: 2, arca: {marca: 'LG', modelo: 'xpto'}};
+ 
+  constructor() {
+    console.log("CASA", this.casa);
+    console.log("CASA-Arca", this.casa.arca);
+    console.log("CASA-arca-modelo", this.casa.arca['modelo']);
+    console.log("CASA-porta", this.casa.porta);
+    console.log("CASA-quant-porta", this.casa.quantPorta);
+   
+  }
+  
 }
