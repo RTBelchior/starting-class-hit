039a83372db81e28d3edc1c89ad8b6ce2b6cb718
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-05-30 11:57:32 +0100

    Inicio da aula 5

--- starting/src/app/app.config.ts
+++ starting/src/app/app.config.ts
@@ -3,6 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzenSk-FRV0tBG5sl2djyU3pamkIqNeCg",
+  authDomain: "starting-class-hit1.firebaseapp.com",
+  projectId: "starting-class-hit1",
+  storageBucket: "starting-class-hit1.firebasestorage.app",
+  messagingSenderId: "161848047674",
+  appId: "1:161848047674:web:a1e77db341aa2c77d61d03"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
