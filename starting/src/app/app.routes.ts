a3826f1673dcf1baf7680d52127c0a21b5599c1e
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-03 09:28:01 +0100

    inicio da aula 06

--- starting/src/app/app.routes.ts
+++ starting/src/app/app.routes.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [
     {path: '', redirectTo:'home', pathMatch:'full'}, //Rota Padrão
     {path: 'home', loadComponent:() => import('./pages/home/home.component').then(c => c.HomeComponent)}, // loadlazy
     {path: 'class-project', loadComponent:() => import('./pages/class-project/class-project.component').then(c => c.ClassProjectComponent)},
+    {path: 'levelone', loadComponent:() => import('./pages/books/level/level01/level01.component').then(c => c.Level01Component)},
     {path: '**', loadComponent:() => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)}
     //{path: '**', redirectTo: () => {return 'not-found'}},
 ];
