6d292150888b928a308b9abc5edb15ea1426603e
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-03 09:33:52 +0100

    inicio da aula06, criação dos componentes

---
+++ starting/src/app/pages/books/level/level01/dependendency-injection/dependendency-injection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dependendency-injection',
+  imports: [],
+  templateUrl: './dependendency-injection.component.html',
+  styleUrl: './dependendency-injection.component.css'
+})
+export class DependendencyInjectionComponent {
+
+}
