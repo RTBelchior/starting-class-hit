fa7779d6f15ec8f80d6e0b72dc78864a258fba8a
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-05 09:24:03 +0100

    update.header.component.html router children is done

--- starting/src/app/app.routes.ts
+++ starting/src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {path: '', redirectTo:'angular', pathMatch:'full'}, //Rota Padrão
     {path: 'angular', loadComponent:() => import('./pages/angular/angular-component/angular.component').then(c => c.AngularComponent)},
-    
+    {path: 'angular/git', loadComponent: () => import('./pages/angular/git/git.component').then(c => c.GitComponent)},
+    {path: 'hit', loadChildren:() => import('./pages/hit/hit.routes').then(c =>c.hitRoutes)},
+
     {path: 'hit/class-project', loadComponent:() => import('./pages/hit/class-project/class-project.component').then(c => c.ClassProjectComponent)},
 
     {path: '**', loadComponent:() => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)}
