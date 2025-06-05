ad8129ebab08b6433473349e3fd167b19e39d725
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-05 11:56:07 +0100

    página angular concluida por mim e nivelamento redirecionado

--- starting/src/app/app.routes.ts
+++ starting/src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
     {path: 'angular', loadComponent:() => import('./pages/angular/angular-component/angular.component').then(c => c.AngularComponent)},
     {path: 'angular/git', loadComponent: () => import('./pages/angular/git/git.component').then(c => c.GitComponent)},
     {path: 'hit', loadChildren:() => import('./pages/hit/hit.routes').then(c =>c.hitRoutes)},
+    {path: 'level', loadChildren: () => import('./pages/books/level/level.routes').then(c => c.levelRoutes)},
 
     {path: 'hit/class-project', loadComponent:() => import('./pages/hit/class-project/class-project.component').then(c => c.ClassProjectComponent)},
 
