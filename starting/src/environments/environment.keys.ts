dcb9f833e29528d8cd254406b29fdc2966ce25c8
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-09 10:19:15 +0100

    Fim da aula 8

---
+++ starting/src/environments/environment.keys.ts
@@ -0,0 +1,5 @@
+import firebase from "firebase/compat/app";
+
+export const keys = {
+    firebaseApiKey: "AIzaSyDzenSk-FRV0tBG5sl2djyU3pamkIqNeCg",
+}
---
+++ starting/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+import { keys } from "./environment.keys";
+
+export const environment = {
+    production:true,
+    firebaseConfig: {
+    apiKey: keys.firebaseApiKey, 
+    authDomain: "starting-class-hit1.firebaseapp.com",
+      projectId: "starting-class-hit1",
+      storageBucket: "starting-class-hit1.firebasestorage.app",
+      messagingSenderId: "161848047674",
+      appId: "1:161848047674:web:a1e77db341aa2c77d61d03"
+    }
+}
