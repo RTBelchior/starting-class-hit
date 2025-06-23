ca88e4f602a23589a30506bf896097a38219637b
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-23 10:27:07 +0100

    fixing  css reqres.component.css

--- starting/src/app/pages/ApIs/reqres/reqres.component.ts
+++ starting/src/app/pages/ApIs/reqres/reqres.component.ts
@@ -1,6 +1,6 @@
 import { httpResource } from '@angular/common/http';
 import { ReqresService } from './../../../_services/reqres.service';
-import { Component, Inject } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IReqRes } from '../../../_shared/interfaces/reqres';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './reqres.component.css'
 })
 export class ReqresComponent {
-  reqresService = Inject(ReqresService);
+  reqresService = inject(ReqresService);
   localReqRes$!: Observable<IReqRes>;
 
   ngOnInit(): void {
