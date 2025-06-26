8ecae3523da9b3626779e7693aa42f614f3c42c3
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-26 10:38:19 +0100

    directives.component, destructuring.component

--- starting/src/app/pages/books/level02/destructuring/destructuring.component.ts
+++ starting/src/app/pages/books/level02/destructuring/destructuring.component.ts
@@ -4,8 +4,41 @@ import { Component } from '@angular/core';
   selector: 'app-destructuring',
   imports: [],
   templateUrl: './destructuring.component.html',
-  styleUrl: './destructuring.component.css'
+  styleUrl: './../../level04/promises/promises.component.css'
 })
 export class DestructuringComponent {
+protected nome!: string;
+protected idade!: string | number;
+protected apelido!: string;
+protected email!:string;
+protected email2!:string;
+protected primeiro!: number;
+protected segundo!: number;
+
+
+  constructor() {
+    const pessoa = {
+      nome: "Belchior",
+      idade: 40,
+      cidade:"Santana",
+      apelido: "mutiz",
+      email: " oficinadosDrones@gmail.com"
+    };
+    // Desestuturação com nome igual
+    ({nome: this.nome, idade: this.idade} = pessoa)
+    console.log("Destruturação com nome igual: ", this.nome, this.idade);
+    
+    // Destruturação com valor padrão
+    ({apelido: this.apelido, email:this.email2 ="segundo@gmail.com"} = pessoa)
+    console.log("Destruturação com valor padrão: ", this.apelido, this.email2);
+    
+    //array
+    const numeros = [10,20,30];
+    
+    [this.primeiro, this.segundo] = numeros;
+    console.log("Array 1º: ", this.primeiro,' 2º: ', this.segundo);
+    
+
+  }
 
 }
--- starting/src/app/pages/books/level02/spread/spread.component.ts
+++ starting/src/app/pages/books/level02/spread/spread.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-spread',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './spread.component.html',
-  styleUrl: './spread.component.css'
+  styleUrl: './../../level04/promises/promises.component.css'
 })
 export class SpreadComponent {
+  
 
 }
