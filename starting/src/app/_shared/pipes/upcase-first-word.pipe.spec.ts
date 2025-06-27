3b310530f687649c5cd89306ca2b3be089528cdd
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-06-27 09:14:50 +0100

    pipes e regex

---
+++ starting/src/app/_shared/pipes/upcase-first-word.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { UpcaseFirstWordPipe } from './upcase-first-word.pipe';
+
+describe('UpcaseFirstWordPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UpcaseFirstWordPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
