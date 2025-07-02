45aaf6abf5f52c8332bb8dbc49ae7ff0899114f5
RTBelchior <rodrigo.tavares.belchior@gmail.com>
2025-07-02 08:32:51 +0100

    header update

--- starting/src/app/components/header/header.component.ts
+++ starting/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UpcaseFirstWordPipe } from "../../_shared/pipes/upcase-first-word.pipe";
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, UpcaseFirstWordPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
